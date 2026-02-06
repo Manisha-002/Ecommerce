bb52614e934aa2a8e8201d57a9ab8f11951219ac
Manisha-002 <manisha22704@gmail.com>
2026-02-06 21:06:32 +0530

    Initial commit

---
+++ Ecom/src/test/java/com/Manisha/Ecom/EcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Manisha.Ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
